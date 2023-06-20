95a03e1d99b7c856b216904e629040c2f2d8ce83
Juan Camilo Prada <881798+juankprada@users.noreply.github.com>
2023-06-20 11:27:19 +0900

    Adds initial import.sql script to populate DB

---
+++ altera-shop-inventory/src/main/resources/import.sql
@@ -0,0 +1,30 @@
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-001', 'Chocolate', 'The recommended one', 'Icecream', 150, 30, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-002', 'Vanilla', 'The classic and plain vanilla', 'Icecream', 100, 20, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-003', 'Pistachio', 'New Flavor!', 'Icecream', 200, 10, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-004', 'Strawberry', 'Fruity and tasty', 'Icecream', 120, 10, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-005', 'Coffee', 'Directly from Colombia', 'Beverage', 80, 100, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-006', 'Protein Bar', 'Something Healthy', 'Snack Bar', 280, 10, NOW());
+
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-007', 'Condensed Milk', 'Refreshing and Sweet', 'Shaved Ice', 100, 50, NOW());
+
+insert into items(sku_number, item_name, description, category, price, inventory, created_on)
+VALUES('SKU-008', 'Mango', 'Tropical flavor', 'Shaved Ice', 100, 50, NOW());
+
+
+
+
+
+
